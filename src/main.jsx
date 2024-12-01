bb59d168b772c5c89128d5f3952ebeadfae8d636
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2024-12-01 21:30:44 +0600

    dashboard skeleton

--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,10 @@ const router = createBrowserRouter([
             path: '/',
             element: <Home></Home>
           },
+          {
+            path: 'dashboard',
+            element: 
+          }
         ]
       },
 ]);
